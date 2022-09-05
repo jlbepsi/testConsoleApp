dfa4536ae8108993372cb97282efbe4f30f373c9
JLB Epsi <jlb.epsi@gmail.com>
2022-09-05 16:46:55 +0200

    Premier commit

---
+++ ConsoleApp/Dev1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Dev1
+    {
+        Dev1()
+        {
+            Console.WriteLine("Dev1 - ctor - version 2");
+        }
+    }
+}
